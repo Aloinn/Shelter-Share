a753d35da3394bd862c5a2b7941697e315571e7c
Aloinn <46081985+Aloinn@users.noreply.github.com>
2022-05-15 01:08:32 -0400

    Create DonatePage.vue

---
+++ quasar/src/pages/DonatePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page>
+    <google-maps-donator />
+  </q-page>
+</template>
+
+<script lang="ts">
+import GoogleMapsDonator from 'src/components/GoogleMapsDonator.vue';
+import { defineComponent } from 'vue';
+import GoogleMaps from '../components/GoogleMaps.vue';
+export default defineComponent({
+  components: { GoogleMapsDonator },
+  setup() {
+    return {};
+  },
+});
+</script>
