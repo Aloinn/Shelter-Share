09427632f1abc2fae6d39a0e3c72ebce2c53b7a7
Alain Nguyen <46081985+Aloinn@users.noreply.github.com>
2022-05-15 03:07:02 -0400

    Add Styles to overview

--- quasar/src/pages/OverviewPage.vue
+++ quasar/src/pages/OverviewPage.vue
@@ -1,25 +1,74 @@
 <template>
   <q-page>
-    <div class="text-h6 text-center">Overview Page</div>
-
-    <div class="row justify-center">
-      <div class="col">
-        <shelter-list />
+    <div class="text-h6 row justify-center">
+      <q-input
+        class="search-input"
+        label="Search by Shelter, Location or ..."
+        v-model="shelterSearch"
+        @change="updateSearchResults"
+        rounded
+        outlined
+      >
+        <template v-slot:append>
+          <q-icon name="search" class="cursor-pointer" color="primary" />
+        </template>
+      </q-input>
+    </div>
+    <div class="row justify-between items-center q-px-xl q-pt-lg">
+      <div class="text-bold">Available Shelters</div>
+      <div class="row items-center q-gutter-x-sm">
+        <div>Sort by</div>
+        <q-select
+          rounded
+          outlined
+          dense
+          class="sort-input"
+          v-model="setSort"
+          :options="['Volunteer Count', 'Newest']"
+        />
+      </div>
+    </div>
+    <div class="row justify-center q-mt-xl">
+      <div class="col-8">
+        <shelter-list :shelterList="shelters" />
+      </div>
+      <div class="col-4">
+        <div>Past Donations</div>
       </div>
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import ShelterList from 'src/components/ShelterList.vue';
+import { useFirestoreCollection } from '@gcto/firebase-hooks/lib';
+import { Shelter } from 'src/components/models';
 
 export default defineComponent({
   setup() {
-    return {};
+    const setSort = ref('Volunteer Count');
+    const shelterSearch = ref('');
+    const shelters = useFirestoreCollection<Shelter>('shelters');
+    function updateSearchResults() {
+      return 0;
+    }
+    return {
+      setSort,
+      shelterSearch,
+      shelters,
+      updateSearchResults,
+    };
   },
   components: { ShelterList },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.search-input {
+  width: 550px;
+}
+.sort-input {
+  max-width: 160px;
+}
+</style>
