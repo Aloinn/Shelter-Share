641092d1f0ee24724cc285e0574379fb6e334f67
Aloinn <46081985+Aloinn@users.noreply.github.com>
2022-05-15 03:02:34 -0400

    Cool map aniamtion

--- quasar/src/components/GoogleMapsDonator.vue
+++ quasar/src/components/GoogleMapsDonator.vue
@@ -1,16 +1,65 @@
 <template>
   <div class="fit">
     <div class="absolute top q-mx-auto row full-width">
-      <div class="row q-mx-auto q-mt-lg">
+      <div class="column search q-mx-auto q-mt-lg">
+        {{ selectedShelter }}
         <q-input
           outline
+          v-model="search"
           class="bg-white fn-md search border-round q-pa-md i"
           placeholder="Search Shelter"
         >
           <q-icon name="search" size="30px" class="q-my-auto" />
         </q-input>
+        <div class="column">
+          <div
+            class="bg-blur q-mt-md border-round clickable"
+            v-for="result in searchResults"
+            :key="result.name"
+          >
+            <q-img
+              class="border-round"
+              height="140px"
+              :src="result.img"
+              @click="() => updateLocation(result)"
+            >
+              <div class="fit col column">
+                <div class="op-80 fn-md fn-800">{{ result.name }}</div>
+                <div class="op-60 fn-sm fn-800">{{ result.address }}</div>
+
+                <div>
+                  <a
+                    class="fn-link text-primary fn-800 op-60"
+                    :href="result.url"
+                    >{{ result.url }}</a
+                  >
+                </div>
+              </div>
+            </q-img>
+            <!-- <div class="row fit q-col-gutter-x-sm">
+              <div class="col-4">
+                <div class="fit">
+                  <q-img
+                    class="border-round"
+                    height="100px"
+                    :src="result.img"
+                  />
+                </div>
+              </div>
+              <div class="col column q-mt-sm">
+                <div class="op-80 fn-md fn-800">{{ result.name }}</div>
+                <div class="op-60 fn-sm fn-800">{{ result.address }}</div>
+                <q-space />
+                <a class="fn-link text-primary fn-600 op-60" href="">{{
+                  result.url
+                }}</a>
+              </div>
+            </div> -->
+          </div>
+        </div>
       </div>
     </div>
+
     <GoogleMap
       api-key="AIzaSyAkCD51UGRDCEgBqC5ZeBCvSW5eq7gmnYw"
       :styles="googleMapStyle"
@@ -26,13 +75,11 @@
         @click="
           () => {
             openDialog(location);
-            log();
           }
         "
-        label="TEST"
         :options="{
-          name: 'TEST',
           icon: 'help-icon.png',
+
           position: {
             lat: location.lat,
             lng: location.long,
@@ -48,13 +95,13 @@
   z-index: 200;
 }
 .search {
-  min-width: 500px;
+  width: 500px;
 }
 </style>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
-import { GoogleMap, Marker, Circle } from 'vue3-google-map';
+import { computed, defineComponent, ref, watch } from 'vue';
+import { GoogleMap, Marker } from 'vue3-google-map';
 import { googleMapStyle } from '../lib/google-map-style';
 import { useFirebaseUser, useFirestoreCollection } from '@gcto/firebase-hooks';
 import { Shelter } from './models';
@@ -65,12 +112,14 @@ import { addDoc, collection, getFirestore } from 'firebase/firestore';
 export default defineComponent({
   components: { GoogleMap, Marker },
   setup() {
+    const selectedShelter = ref('');
     const $q = useQuasar();
     const db = getFirestore();
     const user = useFirebaseUser();
     const options = {
       mapTypeControl: false,
     };
+
     const openDialog = (shelter: Shelter) => {
       $q.dialog({
         component: DialogWrapperVue,
@@ -83,6 +132,21 @@ export default defineComponent({
         });
       });
     };
+
+    const search = ref('');
+    watch(search, () => {
+      if (selectedShelter.value != '' && search.value != '')
+        selectedShelter.value = '';
+    });
+    const searchResults = computed(() =>
+      search.value == ''
+        ? []
+        : shelterLocations.value.filter(
+            (item) =>
+              item?.name?.toLowerCase().includes(search.value.toLowerCase()) ||
+              item?.address?.toLowerCase().includes(search.value.toLowerCase())
+          )
+    );
 
     const shelters = useFirestoreCollection<Shelter>('shelters');
     // COMPUTES ARRAY OF FIRESTORE DOCUMENTS WITH DOC ID
@@ -106,6 +170,16 @@ export default defineComponent({
       console.log('T');
     };
 
+    const updateLocation = (_location: {
+      lat: number;
+      long: number;
+      id: string;
+    }) => {
+      location.value = { lat: _location.lat, lng: _location.long };
+      search.value = '';
+      selectedShelter.value = _location.id;
+    };
+
     const cc = {
       center: location.value,
       radius: 200,
@@ -117,6 +191,8 @@ export default defineComponent({
       clickable: true,
     };
     return {
+      search,
+      searchResults,
       shelters,
       googleMapStyle,
       shelterLocations,
@@ -125,6 +201,8 @@ export default defineComponent({
       openDialog,
       log,
       options,
+      updateLocation,
+      selectedShelter,
     };
   },
 });
